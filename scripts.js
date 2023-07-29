72fc06bb9a35f40709b68abb9c85a3fd4119561f
JhonAragoon <133818945+JhonAragoon@users.noreply.github.com>
2023-07-29 11:24:24 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
+function showRegisterForm() {
+  var loginForm = document.querySelector('.login-form');
+  var registerForm = document.querySelector('.register-form');
 
+  loginForm.style.display = 'none';
+  registerForm.style.display = 'block';
+}
+
+function showLoginForm() {
+  var loginForm = document.querySelector('.login-form');
+  var registerForm = document.querySelector('.register-form');
+
+  loginForm.style.display = 'block';
+  registerForm.style.display = 'none';
+}
