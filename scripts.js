2f175ec6084a4710b8f6841bd9663823e47265d0
JhonAragoon <133818945+JhonAragoon@users.noreply.github.com>
2023-07-30 13:09:36 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
+function simulateLogin() {
+  var usernameInput = document.getElementById('username');
+  var passwordInput = document.getElementById('password');
+  var username = usernameInput.value;
+  var password = passwordInput.value;
+
+  // Simulación de verificación de credenciales
+  if (username === 'usuario' && password === 'contraseña') {
+    alert('Inicio de sesión exitoso.');
+    // Aquí podrías redirigir al usuario a otra página después del inicio de sesión exitoso
+  } else {
+    alert('Usuario o contraseña incorrectos. Por favor, intenta nuevamente.');
+  }
+}
+
+function simulateRegistration() {
+  // Aquí podrías agregar la lógica para simular el registro de un nuevo usuario
+  alert('Registro exitoso. Ahora puedes iniciar sesión.');
+  // Luego del registro exitoso, podrías redirigir al usuario al formulario de inicio de sesión
+  showLoginForm();
+}
+
 function showRegisterForm() {
   var loginForm = document.querySelector('.login-form');
   var registerForm = document.querySelector('.register-form');
@@ -12,4 +34,16 @@ function showLoginForm() {
 
   loginForm.style.display = 'block';
   registerForm.style.display = 'none';
+}
+
+// Event listeners para los botones de inicio de sesión y registro
+var loginButton = document.querySelector('#login-form button[type="button"]');
+var registerButton = document.querySelector('.register-form button[type="button"]');
+
+if (loginButton) {
+  loginButton.addEventListener('click', simulateLogin);
+}
+
+if (registerButton) {
+  registerButton.addEventListener('click', simulateRegistration);
 }
